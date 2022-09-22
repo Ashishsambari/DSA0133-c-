15a26dc9d6ad76eb70148c309590da2c2633a23d
Ashishsambari <112492366+Ashishsambari@users.noreply.github.com>
2022-09-22 09:47:05 +0530

    Update employee dept,cpp.cpp

--- employee dept,cpp.cpp
+++ employee dept,cpp.cpp
@@ -1,29 +1,48 @@
+#include <windows.h>
 #include <iostream>
+
 using namespace std;
 
-class Distance {
-    private:
-        int meter;
-        
-        // friend function
-        friend int addFive(Distance);
+class employee
+{
+	int   emp_number;
+	char  emp_name[20];
+	char emp_dept_name[20];
+	char emp_designation[20];
+	public:
 
-    public:
-        Distance() : meter(0) {}
-        
+		void get_emp_details();
+		void show_emp_details();
 };
 
-// friend function definition
-int addFive(Distance d) {
-
-    //accessing private members from the friend function
-    d.meter += 5;
-    return d.meter;
+void employee :: get_emp_details()
+{
+	cout<<"\nEnter employee number: ";
+	cin>>emp_number;
+	cout<<"\nEnter employee name: ";
+	cin>>emp_name;
+	cout<<"\nEnter employee dept name: ";
+	cin>>emp_dept_name;
+	cout<<"\nEnter employee designation: ";
+	cin>>emp_designation;
+}
+void employee :: show_emp_details()
+{
+	cout<<"\n\n**** Details of  Employee ****";
+	cout<<"\nEmployee number       :  "<<emp_number;
+	cout<<"\nEmployee Name         :  "<<emp_name;
+	cout<<"\nEmployee dept name    :  "<<emp_dept_name;
+	cout<<"\nEmployee designation  :  "<<emp_designation;
+	cout<<"\n-------------------------------\n\n";
 }
 
-int main() {
-    Distance D;
-    cout << "Distance: " << addFive(D);
+
+int main()
+{
+    employee emp;
+    
+    emp.get_emp_details();
+    emp.show_emp_details();
+
     return 0;
 }
-
